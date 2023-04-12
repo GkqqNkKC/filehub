169aeae6d0f88c942818ea6b649e80de4f3a1636
Ioan Daniel <jakarg12@gmail.com>
2023-04-12 15:10:21 +0300

    Back-End and Front-End Example

--- server_src/server.js
+++ server_src/server.js
@@ -1,32 +1,40 @@
 // Load HTTP module
 const http = require("http");
+const cors = require("cors");
+
 
 const hostname = "127.0.0.1";
-const port = 8000;
-
-// Create HTTP server
-const server = http.createServer(function (req, res) {
-    // Set the response HTTP header with HTTP status and Content type
-    res.writeHead(200, { "Content-Type": "text/plain" });
-
-    // Send the response body "Hello World"
-    res.end("Hello World\n");
-});
-
-// Prints a log once the server starts listening
-server.listen(port, hostname, function () {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+const port = 7523;
 
 const express = require("express");
 const app = express();
-const port2 = 3000;
 
-app.get("/test", function (req, res) {
-    res.send("Hello World!");
+app.use(cors());
+
+const sqlite3 = require('sqlite3').verbose();
+
+// open the database
+let db = new sqlite3.Database('../db/db.db');
+
+let sql_shared = `SELECT id "id", content "content" FROM test
+           WHERE shared = 1
+           ORDER BY 1`;
+
+app.get("/shared", function (req, res) {
+
+    db.all(sql_shared, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+
+        console.log(rows);
+        res.send(rows);
+    });
+
+    console.log("Received a GET request at http://127.0.0.1:7523/shared");
 });
 
-app.listen(port, function () {
-    console.log(`Example app listening on port ${port2}!`);
+app.listen(port, hostname, function () {
+    console.log(`App listening on http://${hostname}:${port}/shared`);
 });
 
