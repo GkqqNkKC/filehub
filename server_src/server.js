cac57ad326f72c42474534bd15d09556d5aa46a7
Ioan Daniel <jakarg12@gmail.com>
2023-04-25 23:37:33 +0300

    added magic stuff in backend and new README

--- server_src/server.js
+++ server_src/server.js
@@ -16,7 +16,10 @@ let db = new sqlite3.Database('../db/db.db');
 
 // register function to add a new user
 app.post('/register', (req, res) => {
-    const { email, password } = req.body;
+    const {
+        email,
+        password
+    } = req.body;
     const sql = 'INSERT INTO users (email, password) VALUES (?, ?)';
     db.run(sql, [email, password], function (err) {
         if (err) {
@@ -27,9 +30,31 @@ app.post('/register', (req, res) => {
     });
 });
 
+// login function to verify the user credentials
+app.post('/login', (req, res) => {
+    const {
+        email,
+        password
+    } = req.body;
+    const sql = 'SELECT * FROM users WHERE email = ? AND password = ?';
+    db.get(sql, [email, password], function (err, row) {
+        if (err) {
+            res.status(400).send('Error logging in');
+        } else if (row === undefined) {
+            res.status(401).send('Invalid email or password');
+        } else {
+            res.status(200).send('Logged in successfully');
+        }
+    });
+});
+
 // function to add a comment to a file
 app.post('/add-comment', (req, res) => {
-    const { file, user, str } = req.body;
+    const {
+        file,
+        user,
+        str
+    } = req.body;
     const sql = 'INSERT INTO file_comments (file, user, str) VALUES (?, ?, ?)';
     db.run(sql, [file, user, str], function (err) {
         if (err) {
@@ -42,21 +67,22 @@ app.post('/add-comment', (req, res) => {
 
 // function to upload a file
 app.post('/upload-file', (req, res) => {
-    const { email, password, owner } = req.body;
+    const {
+        email,
+        password
+    } = req.body;
     const timestamp = Date.now();
-    const db_path = 'db/files/${timestamp}';
-    const local_path = '../${db_path}';
+    const db_path = `db/files/${timestamp}`;
+    const local_path = `../${db_path}`;
     const userSql = 'SELECT * FROM users WHERE email = ? AND password = ?';
     db.get(userSql, [email, password], function (err, row) {
         if (err) {
             res.status(400).send('Error uploading file');
         } else if (row === undefined) {
             res.status(401).send('Unauthorized user');
-        } else if (row.id !== owner) {
-            res.status(403).send('Forbidden');
         } else {
             const sql = 'INSERT INTO files (owner, path) VALUES (?, ?)';
-            db.run(sql, [owner, db_path], function (err) {
+            db.run(sql, [row.id, db_path], function (err) {
                 if (err) {
                     res.status(400).send('Error uploading file');
                 } else {
@@ -74,8 +100,25 @@ app.post('/upload-file', (req, res) => {
     });
 });
 
+// function to download a file
+app.get('/files/:timestamp', (req, res) => {
+    const timestamp = req.params.timestamp;
+    const db_path = `db/files/${timestamp}`;
+    const local_path = `../${db_path}`;
+    fs.access(local_path, fs.constants.F_OK, (err) => {
+        if (err) {
+            res.status(404).send('File not found');
+        } else {
+            res.download(local_path, (err) => {
+                if (err) {
+                    res.status(400).send('Error downloading file');
+                }
+            });
+        }
+    });
+});
 
 // start the server
 app.listen(port, hostname, function () {
-    console.log('App listening on http://${hostname}:${port}');
+    console.log(`App listening on http://${hostname}:${port}`);
 });
