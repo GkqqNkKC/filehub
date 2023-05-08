df4ffa16f14ea0dcb2d0e7a5e05ebd20e2bd37fd
Ioan Daniel <jakarg12@gmail.com>
2023-05-08 22:20:30 +0300

    corrected error

--- server_src/server.js
+++ server_src/server.js
@@ -102,6 +102,12 @@ app.post('/upload-file', (req, res) => {
                             const get_user_id = `select id from users where email = ${email}`
 
                             //get the id of the user and store it in the variable user_id
+                            db.get(get_user_id, (err, row) => {
+                                if (err) {
+                                    console.log(`- upload-file: error: when getting user id`);
+                                    res.status(400).send('Error uploading file');
+                                } else {
+                                    const user_id = row.id;
 
                             const permissionSql = `INSERT INTO file_permissions (file, user, permission) VALUES (${this.lastID}, ${user_id}, "rw")`;
                             db.run(permissionSql, function (err) {
